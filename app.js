dc4dd187449a5e091330d1cce54157ea5932ba31
Josh Chough <joshchough@gmail.com>
2020-06-06 23:43:17 -0400

    Added change location

--- app.js
+++ app.js
@@ -4,7 +4,27 @@ const weather = new Weather();
 // Init UI object
 const ui = new UI();
 
-// Set new location
+// UI vars
+const UIcity = document.querySelector('#city');
+const UIstate = document.querySelector('#state');
+const UIbtn = document.querySelector('#w-change-btn');
+
+// Change location button event listener
+UIbtn.addEventListener('click', changeLocation);
+
+// Change location
+function changeLocation(e) {
+  if (UIcity.value !== '' && UIstate.value !== '') {
+    const location = {
+      city: UIcity.value,
+      state: UIstate.value,
+    };
+    setLocation(location, 'CityState');
+    $('#locModal').modal('hide');
+  }
+}
+
+// Set location weather data
 function setLocation(location, type) {
   // Make http request
   weather.getData(location, type).then((data) => {
